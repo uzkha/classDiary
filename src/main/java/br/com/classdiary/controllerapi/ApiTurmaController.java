5a44c503ee4895f2086daad64dabb7b89ca0d326
Diogo <diogolbx@gmail.com>
2015-06-07 22:00:01 -0300

    Iniciado classe API de Turmas para implementação de metodos a serem consumidos via REST para pesquisa da dados da aplicação.

---
+++ src/main/java/br/com/classdiary/controllerapi/ApiTurmaController.java
@@ -0,0 +1,39 @@
+package br.com.classdiary.controllerapi;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import br.com.classdiary.model.Turma;
+import br.com.classdiary.service.TurmaService;
+
+
+@Controller
+@RequestMapping(value= "/api/turma")
+public class ApiTurmaController {
+	
+	@Autowired
+	private TurmaService turmaService;
+	
+	@RequestMapping(method=RequestMethod.GET, produces="application/json", value="getTurmas")
+	@ResponseBody
+    public  Collection<Turma> getTurmas() {
+		return turmaService.listar();
+    }
+	
+	@RequestMapping(method=RequestMethod.GET, produces="application/json", value="getTurmasByName")
+	@ResponseBody
+    public  List<Turma> getTurmasByName(@PathVariable("nome") String nome) {
+		return null;
+    }
+	
+
+	
+}
